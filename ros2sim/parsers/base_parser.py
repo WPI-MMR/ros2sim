9782ecf2b7f95c46d60b61b551f5e4eb60c554f1
Ankur Gupta <rukna1000@gmail.com>
2021-02-02 05:51:12 -0500

    Add abstract base parser

---
+++ ros2sim/parsers/base_parser.py
@@ -0,0 +1,26 @@
+from abc import ABC, abstractmethod
+from typing import Text
+
+import gym
+
+
+class Parser(ABC):
+  """A generic parser class."""
+  def __init__(self, env):
+    self.env = env
+
+  @abstractmethod
+  def parse(input_str: Text) -> Text:
+    """React to an input string passed from the master device.
+
+    As most robot communications are in the master-slave design pattern, this
+    function will most likely just be a state machine that reacts to the 
+    master node.
+
+    Args:
+      input_str (Text): Encoded string from master to decode.
+
+    Returns:
+      Text: A response (if needed--else can return an empty string)
+    """
+    pass
---
+++ ros2sim/parsers/test_base_parser.py
@@ -0,0 +1,19 @@
+import unittest
+
+from ros2sim.parsers import Parser
+
+
+class DummyParser(Parser):
+  def parse(input_str):
+    pass
+
+
+
+class TestBasicParser(unittest.TestCase):
+  def test_construction(self):
+    env = 'test'
+    parser = DummyParser(env)
+    self.assertEqual(env, parser.env)
+
+if __name__ == '__main__':
+  unittest.main()
