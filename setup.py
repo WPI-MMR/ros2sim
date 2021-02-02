f88ad68cc6c355451afea31f9d3da15e671b197c
Ankur Gupta <rukna1000@gmail.com>
2021-02-02 07:40:34 -0500

    Update module requirements

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup
 
 setup(name='ros2sim',
       version='0.0.1',
-      install_requires=[],
+      install_requires=['pyserial'],
       extras_require={
-        'test': []
+        'test': ['parameterized']
       }
 )
