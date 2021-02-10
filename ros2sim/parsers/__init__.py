cc18cafdf564bc77dc64201a9f33c7fdd2e6e8c9
Revant Mahajan <revantmahajan1999@gmail.com>
2021-02-09 23:08:25 -0500

    State machine completed. Still to be tested

--- ros2sim/parsers/__init__.py
+++ ros2sim/parsers/__init__.py
@@ -7,6 +7,7 @@ class special(Enum):
   OBS_REQUEST = b'OBS'
   OK = b'OK'
 
+
 class SerialReadState(Enum):
   INIT = auto()
   READ_PREAMBLE = auto()
@@ -19,6 +20,7 @@ class SerialReadState(Enum):
   READ_R_SHOULDER = auto()
   READ_R_ELBOW = auto()
   READ_CHECKSUM = auto()
+
 
 class JointInformation():
   """Contains the joint information received from serial connection
@@ -38,6 +40,7 @@ class JointInformation():
     self.left_shoulder = 0
     self.left_elbow = 0
     self.right_shoulder = 0
+    self.right_elbow = 0
     self.checksum = 0
     self.checksum_error = False
     self.packet_available = False
@@ -50,6 +53,44 @@ class JointInformation():
     'data_request: {}'.format(self.left_hip, self.left_knee, self.right_hip,
     self.right_knee, self.left_shoulder, self.left_elbow, self.right_shoulder,
     self.checksum, self.checksum_error, self.packet_available, self.data_request)
+
+  def set_joint_value_from_state(self, s: SerialReadState, value: int):
+    """Sets the joint value for the given SerialReadState
+    """
+    if s == SerialReadState.READ_L_HIP:
+      self.left_hip = value
+    elif s == SerialReadState.READ_L_KNEE:
+      self.left_knee = value
+    elif s == SerialReadState.READ_R_HIP:
+      self.right_hip = value
+    elif s == SerialReadState.READ_R_KNEE:
+      self.right_knee = value
+    elif s == SerialReadState.READ_L_SHOULDER:
+      self.left_shoulder = value
+    elif s == SerialReadState.READ_L_ELBOW:
+      self.left_elbow = value
+    elif s == SerialReadState.READ_R_SHOULDER:
+      self.right_shoulder = value
+    elif s == SerialReadState.READ_R_ELBOW:
+      self.right_elbow = value
+
+  def get_joint_value_from_state(self, s: SerialReadState) -> int:
+    if s == SerialReadState.READ_L_HIP:
+      return self.left_hip
+    elif s == SerialReadState.READ_L_KNEE:
+      return self.left_knee
+    elif s == SerialReadState.READ_R_HIP:
+      return self.right_hip
+    elif s == SerialReadState.READ_R_KNEE:
+      return self.right_knee
+    elif s == SerialReadState.READ_L_SHOULDER:
+      return self.left_shoulder
+    elif s == SerialReadState.READ_L_ELBOW:
+      return self.left_elbow
+    elif s == SerialReadState.READ_R_SHOULDER:
+      return self.right_shoulder
+    elif s == SerialReadState.READ_R_ELBOW:
+      return self.right_elbow 
 
 
 from .base_parser import Parser
--- ros2sim/sims/serial.py
+++ ros2sim/sims/serial.py
@@ -2,6 +2,7 @@ import pty
 import os
 import serial
 import threading
+import copy
 
 from ros2sim.parsers import Parser
 from ros2sim.parsers import special as s
@@ -22,12 +23,11 @@ class SerialSimulator:
     self.temp_packet_data = JointInformation()
     self.validated_packed_data = JointInformation()
     self.serial_read_state = SerialReadState.INIT
-    self.serial_port_name = None
+    self.list_of_states = [state for state in SerialReadState]
 
   def serve(self):
     self.master, slave = pty.openpty()
     s_name = os.ttyname(slave)
-    self.slave = slave
 
     thread = threading.Thread(target=self.listener)
     thread.start()
@@ -44,6 +44,15 @@ class SerialSimulator:
     """
     while True:
       self.recv_data()
+      if self.validated_packed_data.packet_available:
+        print(self.validated_packed_data)
+        self.validated_packed_data.packet_available = False
+        if validated_packed_data.request:
+          print("Requesting data")
+        else:
+          print("Update Joint Angles")
+          print(self.validated_packed_data)
+
     # while True:
     #   request = b''
     #   while not request.endswith(s.EOM.value):
@@ -56,7 +65,6 @@ class SerialSimulator:
     """Checks to see if there is any data over the serial port to read. If there is,
     this method reads it and fills up the corresponding data structures. 
     """
-    
     # Counter to check for the number of ints in preamble
     preamble_counter = self.PREAMBLE_LENGTH
     # Counter to check the number of for each joint angle
@@ -64,20 +72,94 @@ class SerialSimulator:
     # Running checksum used to determine the validity of the incoming packet.
     # We don't expect the mocking serial to have issues with the packed validity
     # but this is being implemented to keep with consistency of the Arduino code
-    calculated_checksum = 0xFF
-    # Data that is going to be read of serial. This is going to block here untill it reads.
+    calculated_checksum = 255
+    # Data that is going to be read of serial. This data is read as BYTES 
+    # This is going to block here untill it reads.
     # Arduino has a non blocking version of this same code.
     # TODO: Evaluate if it can be made non blocking
     received_data = os.read(self.master, 1)
+    # Converting the data from bytes to int
+    received_data = int(received_data.hex(), 16)
+
+    # TODO: Andrew what is the range of data being transferred.
+
 
     if self.serial_read_state == SerialReadState.INIT:
       # Initializing checksum
-      # TODO: Duplicated from the start of the method. Maybe remove from ther 
-      calculated_checksum = 0x00
+      # TODO: Duplicated from the start of the method. Maybe remove from there
+      calculated_checksum = 0
       preamble_counter = self.PREAMBLE_LENGTH
       # Reset the temp packet to store values in. We decide after reading checksum if the data
       # received is valid
       self.temp_packet_data.set_to_default()
+      # Changing serial read state to read preamble
+      self.serial_read_state = SerialReadState.READ_PREAMBLE
+
+    elif self.serial_read_state == SerialReadState.READ_PREAMBLE:
+      if received_data == 255:
+        preamble_counter -= 1
+        if preamble_counter == 0:
+          preamble_counter = self.PREAMBLE_LENGTH
+          self.serial_read_state = SerialReadState.READ_L_HIP
+      else:
+        # TODO: Ask Andrew why do we even need this. If it is corrupt,
+        # shouldn't we discard it?
+        preamble_counter = self.PREAMBLE_LENGTH
+
+    elif self.serial_read_state == SerialReadState.READ_L_HIP:
+      # TODO: Ask Andrew about this condition. Also why couldn't we add a extra byte in the protocol.
+      # The request is to get back the robot state
+      if data_byte_counter == 1 and temp_packet_data.left_hip == received_data:
+        self.temp_packet_data.data_request = True
+        self.temp_packet_data.left_hip += received_data
+        calculated_checksum += received_data
+        data_byte_counter = self.DATA_BYTE_LENGTH
+        self.serial_read_state = SerialReadState.READ_CHECKSUM
+      else:
+        self.temp_packet_data.left_hip += received_data
+        calculated_checksum += received_data
+        data_byte_counter -= 1
+        if data_byte_counter == 0:
+          data_byte_counter = self.DATA_BYTE_LENGTH
+          self.serial_read_state = SerialReadState.READ_L_KNEE
+    
+    # Reading all the joint states except READ_L_HIP
+    elif self.serial_read_state in self.list_of_states[3:10]:
+
+      val = self.temp_packet_data.get_joint_value_from_state(self.serial_read_state)
+      val += received_data
+      self.temp_packet_data.set_joint_value_from_state(self.serial_read_state, val)
+
+      calculated_checksum += received_data
+      data_byte_counter -= 1
+      if data_byte_counter == 0:
+        data_byte_counter=self.DATA_BYTE_LENGTH
+        self.serial_read_state = self.list_of_states[self.list_of_states.index(self.serial_read_state) + 1]
+
+    elif self.serial_read_state == SerialReadState.READ_CHECKSUM:
+      self.temp_packet_data.checksum = received_data
+
+      if self.temp_packet_data == 255 - (calculated_checksum % 256):
+        # This is a good packet
+
+        # TODO: I don't think we need this here if we go to INIT next
+        calculated_checksum = 0
+        preamble_counter = self.PREAMBLE_LENGTH
+
+        self.temp_packet_data.checksum_error = False
+        self.temp_packet_data.packet_available = True
+
+        self.validated_packed_data = copy.deepcopy(self.temp_packet_data)
+      else:
+        self.temp_packet_data.checksum_error = True
+        self.temp_packet_data.packet_available = False
+
+      self.serial_read_state = SerialReadState.INIT
+
+
+
+
+
 
 
 
