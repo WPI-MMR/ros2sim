41a3532c646736bae6f7cc56fa59e3852dc73b2b
Revant Mahajan <revantmahajan1999@gmail.com>
2021-02-27 17:36:11 -0500

    adding numpy for install_requires

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages, setup
 setup(name='ros2sim',
       version='0.0.1',
       packages=find_packages(),
-      install_requires=['pyserial'],
+      install_requires=['pyserial', 'numpy'],
       extras_require={
         'test': ['parameterized']
       }
